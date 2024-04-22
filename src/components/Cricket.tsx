01d3520f1db76985a8c21825845b106759ac45dd
Doruk Tuna <doruktuna@gmail.com>
2024-04-22 15:18:18 +0300

    Initial commit

---
+++ src/components/Cricket.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Cricket() {
+
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default Cricket
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Cricket from './components/Cricket';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Cricket />
+  </React.StrictMode>,
+)
